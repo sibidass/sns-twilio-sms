50316df3d0780458d1c730423d44260d9fcb34b3
sibidas <sibidas@gmail.com>
2018-10-23 12:22:47 +0530

    enabling commented out send_sms call

---
+++ lambda/twilio_config.py
@@ -0,0 +1,5 @@
+ACCOUNT_KEY = "XXXXXXXXXXXXXXX"
+ACCOUNT_TOKEN = "XXXXXXXXXXXXXX"
+URL = "https://notify.twilio.com/v1/Services/ISXXXXXXXXXXXXXX/Notifications"
+SMS = True
+AWS_REGION = "ap-south-1"
